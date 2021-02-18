d455316299bf5571a2a97fbec8bac72f963fb4de
asus <hassan.shokoohimatin@gmail.com>
2021-02-18 23:02:49 +0330

    service (ContactRepository) added

--- src/service/ContactRepository.java
+++ src/service/ContactRepository.java
@@ -1,4 +1,26 @@
 package service;
 
-public class ContactRepository {
+import config.CrudRepository;
+import model.Contact;
+
+public class ContactRepository extends CrudRepository<Contact> {
+
+    private static ContactRepository contactRepository;
+
+    private ContactRepository(){
+    }
+
+    public static ContactRepository getInstance(){
+        if (contactRepository == null)
+            contactRepository = new ContactRepository();
+        return contactRepository;
+    }
+
+    @Override
+    protected Class<Contact> getEntityClass() {
+        return Contact.class;
+    }
 }
+
+
+
